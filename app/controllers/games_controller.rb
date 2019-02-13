f1acea154f7da9792c3d9a1fc593ce70f446eed8
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-13 20:03:01 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,7 +21,13 @@ class GamesController < ApplicationController
  end 
  
  private 
+
+  def game_params
+    params.permit(state: [])
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
  
- 
- 
-end
+end #end of the class 
